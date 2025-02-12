badeb234d5f9684b60ec43d81e2236fc33eaaa89
leengsmile <leengsmile@126.com>
2025-02-12 22:46:36 +0800

    test attn

--- llm/model.py
+++ llm/model.py
@@ -110,6 +110,11 @@ class GPT(nn.Module):
         
         self.block_size = block_size
         
+        self.apply(self._init_weights)
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+        
     def _init_weights(self, modlue):
         if isinstance(module, nn.Linear):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
