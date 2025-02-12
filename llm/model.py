c79fe1badcab8547509f73ff555f25e20bceaee4
leengsmile <leengsmile@126.com>
2025-02-12 22:49:32 +0800

    test attn

--- llm/model.py
+++ llm/model.py
@@ -115,7 +115,7 @@ class GPT(nn.Module):
         self.apply(self._init_weights)
         for pn, p in self.named_parameters():
             if pn.endswith('c_proj.weight'):
-                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * n_layer))
         
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
