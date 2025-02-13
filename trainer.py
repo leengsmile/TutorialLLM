c61940d4384444d799b72084d44cea361ab7186c
leengsmile <leengsmile@126.com>
2025-02-13 17:00:58 +0800

    logging simple loss

--- trainer.py
+++ trainer.py
@@ -74,6 +74,7 @@ class Trainer():
             # Reset the evaluator to clear the loss history for each epoch
             self.evaluator.reset()
             for i, (inputs, labels) in enumerate(self.dataset.get_batch_generator_finetune('train')):
+                print(f'## {inputs.size()}, {labels.size()}')
                 # Forward pass and calculate the loss
                 _, loss = self.model(inputs, labels)
 
